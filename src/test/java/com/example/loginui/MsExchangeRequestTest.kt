c1660cc2a4a42506276b672af7a0b8572337bf81
ASMAEELKHIRAOUI <a.elkhiraoui@ciscod.com>
2025-09-24 17:24:58 +0100

    added MsExchangeRequestTest

--- src/test/java/com/example/loginui/MsExchangeRequestTest.kt
+++ src/test/java/com/example/loginui/MsExchangeRequestTest.kt
@@ -1,4 +1,18 @@
 package com.example.loginui
 
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.loginui.net.MsExchangeRequest
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.Assert.assertEquals
+
+@RunWith(AndroidJUnit4::class)
 class MsExchangeRequestTest {
+
+    @Test
+    fun constructor_and_property_are_covered() {
+        val token = "idTok_123"
+        val req = MsExchangeRequest(token)
+        assertEquals(token, req.idToken)
+    }
 }
